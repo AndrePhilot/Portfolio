a9b0a7c9b2a9e929df222f391c0de59214489e3f
Andre Philot <andrephilot@gmail.com>
2024-01-25 08:53:06 -0600

    Trying to set a customTheme

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -132,7 +132,7 @@ const config = {
     snippetVersion: 6,
   },
   themeConfig: {
-    defaultTheme: 'dark',
+    defaultTheme: 'customTheme',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
