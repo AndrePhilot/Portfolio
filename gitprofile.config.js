927a748a642fa6cebf6e367bf2c2c0cf28c78df9
Andre Philot <andrephilot@gmail.com>
2024-03-08 13:32:25 -0600

    Update resume url again

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -30,7 +30,7 @@ const config = {
   },
   resume: {
     fileUrl:
-      'https://drive.google.com/file/d/1_6u5cgaxEm0bblJQwtlabwkNd6wfA5i8/view?usp=sharing', // Empty fileUrl will hide the `Download Resume` button.
+      'https://drive.google.com/file/d/1ir2scegLgpIF02q2IyF9hgdIyvIGU_r2/view?usp=sharing', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
     'JavaScript',
